49ca16d9f51c690cd6d2a6d0e8e841e42e642e42
jafoor <abujafor.cse11@gmail.com>
2021-03-10 14:51:32 +0600

    first commit

---
+++ 1.basicsOfJs/part-1/script.js
@@ -0,0 +1,55 @@
+/* *****
+variables and data type
+*/
+
+
+/*
+var firstName = 'Jafor';
+console.log(firstName);
+
+var lastName = 'Saleh';
+var age = 22;
+// javascript has pynamic typing: data types are automatically assigned to variable
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = "Learner";
+console.log(job);
+
+var _3years = 3;
+var jhonMark = "Jhon and Mark";
+*/
+
+/* *****
+variable mutation and data type coercion
+*/
+
+
+/*
+var firstName = 'Jafor';
+var age = 22;
+console.log(firstName + ' ' + age);
+// convert number age to string -> it's called type coercion
+
+// type corecion
+var job, isMarried;
+job = 'Student';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' years old ' + job + ' is he married? ' + isMarried);
+
+// variable mutation
+
+age = 'twenty tow';
+job = 'developer';
+alert(firstName + ' is a ' + age + ' years old ' + job + ' is he married? ' + isMarried);
+
+var lastName = prompt("what's your last name?");
+console.log(firstName + ' ' + lastName);
+*/
+
+
