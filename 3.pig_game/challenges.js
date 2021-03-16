46ae4eafdaadb7c2902cfa99414d5f8bcb3d546e
jafoor <abujafor.cse11@gmail.com>
2021-03-16 16:26:42 +0600

    Final pig game

--- 3.pig_game/challenges.js
+++ 3.pig_game/challenges.js
@@ -11,7 +11,7 @@ Game Rule:
 
 
 
-var scores, roundScore, activePlayer, gamePlaying;
+var scores, roundScore, activePlayer, gamePlaying, lastDice, winningScore;
 
 init();
 //dice = Math.floor(Math.random() * 6) + 1;
@@ -27,24 +27,44 @@ document.querySelector('.btn--roll').addEventListener('click', function () {
     if (gamePlaying){
         
         // 1. random number
-    var dice = Math.floor(Math.random() * 6) + 1;
-
+    var dice1 = Math.floor(Math.random() * 6) + 1;
+    var dice2 = Math.floor(Math.random() * 6) + 1;
+        
     // 2. Display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
+    document.getElementById('dice-1').style.display = 'block';
+    document.getElementById('dice-2').style.display = 'block';
+    document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+    document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
     
+    
+    if (dice1 !== 1 && dice2 !== 1){
+        roundScore += dice1 + dice2;
+        document.getElementById('current--' + activePlayer).textContent = roundScore;
+    } else{
+        nextPlayer();
+    }
+    /*
     // 3. Update the round IF the rolled number was 
     
-    if ( dice !== 1) {
+    if (dice === 6 && lastDice == 6){
+        
+        scores[activePlayer] = 0;
+        document.querySelector('#score--' + activePlayer).textContent = 0;
+        
+        nextPlayer();
+        
+    }else if ( dice !== 1) {
         // Add Score
         roundScore += dice;
         document.querySelector('#current--' + activePlayer).textContent = roundScore;
     } else{
         nextPlayer();
     }
-        
+       lastDice = dice;
+       */ 
     }
+    
+    
     
 });
 
@@ -60,14 +80,26 @@ document.querySelector('.btn--hold').addEventListener('click', function () {
     
     // Update the UI
     document.querySelector('#score--' + activePlayer).textContent = scores[activePlayer];
+        
+    var input = document.querySelector('.final-score').value;
+    
+        
+    if (input){
+        winningScore = input;
+    }
     
     // Check if the player won the game
-    if(scores[activePlayer] >= 10) {
+    if(scores[activePlayer] >= winningScore) {
         document.querySelector('#name--' + activePlayer).textContent = 'Winner';
+        
+        document.getElementById('dice-1').style.display = 'none';
+        document.getElementById('dice-2').style.display = 'none';
+        
         document.querySelector('.dice').style.display = 'none';
         document.querySelector('.player--' + activePlayer).classList.add('player--winner');
         document.querySelector('.player--' + activePlayer).classList.remove('player--active');
         gamePlaying = false;
+        
     }else{
         // Next Player
      nextPlayer();
@@ -90,7 +122,8 @@ function nextPlayer() {
         document.querySelector('.player--0').classList.toggle('player--active');
         document.querySelector('.player--1').classList.toggle('player--active');
         
-        document.querySelector('.dice').style.display = 'none';
+        document.getElementById('dice-1').style.display = 'none';
+        document.getElementById('dice-2').style.display = 'none';
     
       
 }
@@ -102,10 +135,11 @@ function init() {
     scores = [0,0];
     roundScore = 0;
     activePlayer = 0;
-    
+    winningScore = 100;
     gamePlaying = true;
     
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
 
 
     document.getElementById('score--0').textContent = '0';
@@ -131,4 +165,4 @@ function init() {
 coding challenges 6
 
 1. A player looses his entire score when he rolls two 6  in a row. After that it's next player turn
-
+*/
