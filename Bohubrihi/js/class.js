22f99d1fd59ad6777cf515968f7a15e0d148708c
jafoor <abujafor.cse11@gmail.com>
2021-04-19 23:51:08 +0600

    Redux

--- Bohubrihi/js/class.js
+++ Bohubrihi/js/class.js
@@ -1,6 +1,8 @@
 // Class(ES6) 
 // Class is a template for creating objects
 
+
+/*
 class Person {
     constructor(fname, lname, birthday) {
         this.firstname = fname;
@@ -48,3 +50,68 @@ console.log(person3.calculateAge());
 console.log(person1.fullname());
 console.log(person2.fullname());
 console.log(person3.fullname());
+*/
+
+/*
+
+// Sub Class
+// Inheritance
+
+class Person { //Base Class
+    constructor(fname, lname) {
+        this.firstname = fname;
+        this.lastname = lname;
+    }
+
+    greeting() {
+        return `Hello ${this.firstname} ${this.lastname}!`;
+    }
+}
+
+class Customer extends Person { //Sub Class
+    constructor(fname, lname, phone, memberShip) {
+        super(fname, lname);
+
+        this.phone = phone;
+        this.memberShip = memberShip;
+    }
+
+    fullname() {
+        console.log(this.firstname, this.lastname);
+    }
+}
+
+let person1 = new Person("Fazle", "Rahat");
+console.log(person1);
+console.log(person1.greeting());
+console.log(person1.fullname());
+
+let customer1 = new Customer("Rony", "Chy", "017777778888", "1234");
+console.log(customer1);
+console.log(customer1.greeting());
+console.log(customer1.fullname());
+
+*/
+
+class Person {
+    constructor(fanme, lname){
+        this.firstName = fanme;
+        this.lastName = lname;
+    };
+
+    fullname (){
+        return `Full Name : ${this.firstName} ${this.lastName}`;
+    };
+
+    static test(){
+        console.log("I am test");
+    }
+};
+
+
+// Method is from objest
+let person1 = new Person("Abu", "Jafor");
+console.log(person1.fullname());
+
+// Static function can not be called from object but class
+Person.test();
---
+++ Bohubrihi/js/regx.js
@@ -0,0 +1,107 @@
+/*
+// Regular Expression
+// Pattern Matching Technique
+let re;
+let str;
+
+re = /hello/;
+re = /Hello/;
+re = /hello/i; // i = case insensitive
+
+console.log(re);
+console.log(re.source);
+
+str = "HelLo World";
+str = "Again Hello World";
+str = "Hell World";
+str = "sdsHellosds World";
+str = "Again Hello World Hello";
+str = "World";
+
+// exec() - Returns result in an array or null
+let result = re.exec(str);
+
+// test() - true/false
+result = re.test(str);
+
+// match() - Returns array or null
+str = "Again Hello World Hello";
+
+result = str.match(re);
+
+// search() - Returns index of the first match or -1
+str = "World";
+
+result = str.search(re);
+
+// replace() - Return new string
+str = "Again Hello World Hello";
+let newstr = str.replace(re, "Hi");
+
+console.log(result);
+console.log(str);
+console.log(re.source);
+console.log(newstr);
+
+*/
+
+
+
+/*
+
+let re;
+let str;
+// Literal Character
+re = /hello/;
+re = /hello/i;
+re = /hell/i;
+re = /lo W/i;
+re = /loW/i;
+
+// Meta Characters
+re = /^hello/; // Must start with
+re = /hello$/; // Must end with
+re = /world$/;
+re = /^hello$/; // Must start and end with
+re = /^h.llo$/; // Matches any one character
+re = /h.llo/;
+re = /h*llo/; // 0 or more times
+re = /he?a?llo/; // Optional
+re = /hello?/; // escaping
+
+str = "Again hello World";
+str = "hello hello";
+str = "hello";
+str = "hallo";
+str = "hillo";
+str = "h llo";
+str = "hllo";
+str = "hillo worlde";
+str = "hello worlde";
+str = "hillo";
+str = "hilo";
+str = "hhfsdhfsuillo";
+str = "hello";
+str = "hllo";
+str = "hallo";
+str = "htllo";
+str = "heallo";
+str = "hello";
+str = "hallo";
+str = "hllo";
+str = "hello";
+str = "hello?";
+
+
+
+console.log(re.exec(str));
+reTest(re, str);
+function reTest(re, str) {
+    if(re.test(str)) {
+        console.log(`'${str}' matches '${re.source}'`);
+    }
+    else {
+        console.log(`'${str}' doesn't match '${re.source}'`);
+    }
+}
+*/
